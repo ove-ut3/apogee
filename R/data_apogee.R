a2bd936d18cbcaa169646379893e28d8811cb042
Stéphane Le Hénanff <github@le-henanff.fr>
2018-05-29 10:39:01 +0200

    data_inscrits_elp : maj de la regex des fichiers csv

--- R/data_apogee.R
+++ R/data_apogee.R
@@ -245,7 +245,7 @@ data_inscrits_elp <- function(derniere_annee = TRUE) {
     n_csv <- Inf
   }
   
-  inscrits_elp <- impexp::csv_importer_masse("Inscrits_ELP.+?\\.csv$", chemin = paste0(racine_packages, "apogee/raw"), archive_zip = TRUE, n_csv = n_csv, ligne_debut = 2) %>% 
+  inscrits_elp <- impexp::csv_importer_masse("Inscrits_ELP.*?\\.csv$", chemin = paste0(racine_packages, "apogee/raw"), archive_zip = TRUE, n_csv = n_csv, ligne_debut = 2) %>% 
     dplyr::transmute(import = lapply(import, source.maj::renommer_champs, impexp::access_importer("_rename", paste0(racine_packages, "apogee/raw/Tables_ref.accdb"))),
                      import = lapply(import, source.maj::transcoder_champs, impexp::access_importer("_contents", paste0(racine_packages, "apogee/raw/Tables_ref.accdb")))) %>% 
     tidyr::unnest() %>% 
