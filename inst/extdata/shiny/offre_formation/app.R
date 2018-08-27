7e55e079506ba1f267ac6e503e5800f6581c94c1
stephLH <github@le-henanff.fr>
2018-08-27 22:43:24 +0200

    Ajout du bouton reset

--- inst/extdata/shiny/offre_formation/app.R
+++ inst/extdata/shiny/offre_formation/app.R
@@ -18,40 +18,52 @@ columnFilterUI <- function(id) {
   uiOutput(ns("filter_container"))
 }
 
-columnFilter <- function(input, output, session, df, col_num, choice_filter) {
-  # This renders a selectInput and only re-renders when the selected data
-  # frame changes. (i.e. it doesn't re-render when filters change state.)
-  output$filter_container <- renderUI({
-    # Don't render if col_num is > actual number of cols
-    req(col_num <= ncol(df()))
-    
-    freezeReactiveValue(input, "filter_value")
-    
-    choices <- sort(unique(df()[,col_num,drop=TRUE]))
-
-    # names(df())[[col_num]]
-    pickerInput(session$ns("filter_value"), cols_name[col_num], multiple = T, 
-                choices = choices, selected = choices,
-                options = list(`actions-box` = T, `select-all-text` = "Tout sélectionner",
-                               `deselect-all-text` = "Tout désélectionner",
-                               `none-selected-text` = "Aucune sélection"))
-
-  })
+columnFilter <- function(input, output, session, df, col_num, choice_filter, reset=F) {
   
-  # When the other filters change, update this filter to remove rows that
-  # are filtered out by the other filters' criteria. (We also add in the
-  # currently selected values for this filter, so that changing other
-  # filters does not cause this filter's selected values to be unselected;
-  # while that behavior might make sense logically, it's a poor user
-  # experience.)
-  observeEvent(choice_filter(), {
-    current_values <- input$filter_value
+  if (reset) {
     
-    updatePickerInput(session, "filter_value",
-                      choices = sort(unique(c(current_values, df()[choice_filter(),col_num,drop=TRUE]))),
-                      selected = current_values
+    updateSelectInput(session, "filter_value",
+                      choices = sort(unique(df()[,col_num,drop=TRUE])),
+                      selected = NULL
     )
-  })
+    
+  } else {
+  
+    # This renders a selectInput and only re-renders when the selected data
+    # frame changes. (i.e. it doesn't re-render when filters change state.)
+    output$filter_container <- renderUI({
+      # Don't render if col_num is > actual number of cols
+      req(col_num <= ncol(df()))
+      
+      freezeReactiveValue(input, "filter_value")
+      
+      choices <- sort(unique(df()[,col_num,drop=TRUE]))
+  
+      # names(df())[[col_num]]
+      pickerInput(session$ns("filter_value"), cols_name[col_num], multiple = T, 
+                  choices = choices, selected = choices,
+                  options = list(`actions-box` = T, `select-all-text` = "Tout sélectionner",
+                                 `deselect-all-text` = "Tout désélectionner",
+                                 `none-selected-text` = "Aucune sélection"))
+  
+    })
+    
+    # When the other filters change, update this filter to remove rows that
+    # are filtered out by the other filters' criteria. (We also add in the
+    # currently selected values for this filter, so that changing other
+    # filters does not cause this filter's selected values to be unselected;
+    # while that behavior might make sense logically, it's a poor user
+    # experience.)
+    observeEvent(choice_filter(), {
+      current_values <- input$filter_value
+      
+      updatePickerInput(session, "filter_value",
+                        choices = sort(unique(c(current_values, df()[choice_filter(),col_num,drop=TRUE]))),
+                        selected = current_values
+      )
+    })
+  
+  }
   
   # Return a reactive that is a row index of selected rows, according to
   # just this filter. If this filter shouldn't be taken into account
@@ -71,9 +83,12 @@ columnFilter <- function(input, output, session, df, col_num, choice_filter) {
 columnFilterSetUI <- function(id, cols) {
   ns <- NS(id)
   
-  lapply(cols, function(i) {
+  list(
+    actionButton(ns("clear_all_filters_button"), "Remise à zéro"),
+    lapply(cols, function(i) {
         columnFilterUI(ns(paste0("col", i)))
     })
+  )
   
 }
 
@@ -89,6 +104,14 @@ columnFilterSet <- function(input, output, session, df, cols) {
       Reduce(`&`, filter_values, TRUE)
     })
   }
+  
+  observeEvent(input$clear_all_filters_button, {
+    cat("clear", "\n")
+    filters <- lapply(cols, function(i) {
+      callModule(columnFilter, paste0("col", i), df= df, col_num =i, create_choice_filter(i), reset=T)
+    })
+  })
+  
   
   # filters is a list of reactive expressions, each of which is a
   # logical vector of rows to be selected.
