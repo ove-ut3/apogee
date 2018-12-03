1d4f50c609857b0b927ebd6f700b7c74b1eda8e6
Stéphane Le Hénanff <github@le-henanff.fr>
2018-12-03 13:26:40 +0100

    Mise à jour des données 03/12/18

--- data-raw/apogee.R
+++ data-raw/apogee.R
@@ -3,6 +3,8 @@ source("data-raw/utils.R")
 # Données brutes
 source("data-raw/apogee-data.R")
 
+developr::package_build()
+
 # Méta-données
 source("data-raw/composante.R")
 source("data-raw/diplome.R")
@@ -12,3 +14,5 @@ source("data-raw/etape.R")
 source("data-raw/inscription.R", encoding = "UTF-8")
 source("data-raw/resultat.R")
 source("data-raw/sise.R")
+
+developr::package_build()
