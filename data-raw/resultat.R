c9b21331b6406a35664752fb129876d032da022f
Stéphane Le Hénanff <github@le-henanff.fr>
2019-06-24 17:58:33 +0200

    Refont de la table resultat : resultat_parent

--- data-raw/resultat.R
+++ data-raw/resultat.R
@@ -2,6 +2,10 @@
 
 resultat <- readxl::read_excel("data-raw/Resultat.xlsx", skip = 1) %>% 
   patchr::rename(apogee::rename) %>% 
-  dplyr::left_join(impexp::access_import("resultat", "data-raw/Tables_ref.accdb"), by = "code_resultat")
+  dplyr::full_join(impexp::access_import("resultat", "data-raw/Tables_ref.accdb") %>% 
+                     dplyr::rename(lib_resultat_maj = lib_resultat),
+                   by = "code_resultat") %>% 
+  dplyr::mutate(lib_resultat = dplyr::if_else(!is.na(lib_resultat_maj), lib_resultat_maj, lib_resultat)) %>% 
+  dplyr::select(-lib_resultat_maj)
 
 usethis::use_data(resultat, overwrite = TRUE)
