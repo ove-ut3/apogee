9ebef285dca01ac6197c3a4c8e39314a9c396c4c
stephLH <github@le-henanff.fr>
2020-03-31 09:06:32 +0100

    table resultat: codes uniquement en base

--- data-raw/scripts/resultat.R
+++ data-raw/scripts/resultat.R
@@ -8,6 +8,14 @@ resultat <- readxl::read_excel("data-raw/data/Resultat.xlsx", skip = 1) %>%
     by = "code_resultat"
   ) %>%
   dplyr::mutate(lib_resultat = dplyr::if_else(!is.na(lib_resultat_maj), lib_resultat_maj, lib_resultat)) %>%
-  dplyr::select(-lib_resultat_maj)
+  dplyr::select(-lib_resultat_maj) %>% 
+  dplyr::semi_join(
+    dplyr::bind_rows(
+      tidyr::drop_na(apogee::resultats_diplome, code_resultat),
+      tidyr::drop_na(apogee::resultats_elp, code_resultat),
+      tidyr::drop_na(apogee::resultats_etape, code_resultat)
+    ),
+    by = "code_resultat"
+  )
 
 usethis::use_data(resultat, overwrite = TRUE)
