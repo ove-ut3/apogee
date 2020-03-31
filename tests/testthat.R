72e9b8f0899512d2bd3a8565f2485283a55dd3e5
stephLH <github@le-henanff.fr>
2020-03-31 08:55:01 +0100

    begin tests

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(apogee)
+
+test_check("apogee")
---
+++ tests/testthat/test-data_duplicate.R
@@ -0,0 +1,16 @@
+test_that("Pas de doublons dans les tables de données", {
+  expect_equal(nrow(patchr::duplicate(individus, code_etudiant)), 0)
+  expect_equal(nrow(patchr::duplicate(individus_diplome_origine, code_etudiant, annee_diplome_obtenu)), 0)
+  expect_equal(nrow(patchr::duplicate(individus_situation_annee_precedente, code_etudiant, annee)), 0)
+  expect_equal(nrow(patchr::duplicate(inscrits, annee, code_etape, code_etudiant, inscription_premiere)), 0)
+  expect_equal(nrow(patchr::duplicate(inscrits_cpge, annee, code_etape, code_etudiant, inscription_premiere)), 0)
+  expect_equal(nrow(patchr::duplicate(inscrits_elp, annee, code_etape, code_etudiant, inscription_premiere, code_elp)), 0)
+  expect_equal(nrow(patchr::duplicate(inscrits_peda, annee, code_etape, code_etudiant, inscription_premiere)), 0)
+  expect_equal(nrow(patchr::duplicate(resultats_elp, annee, code_etape, code_etudiant, inscription_premiere, code_elp, lib_session)), 0)
+  expect_equal(nrow(patchr::duplicate(resultats_etape, annee, code_etape, code_etudiant, inscription_premiere, lib_session)), 0)
+  expect_equal(nrow(patchr::duplicate(resultats_diplome, annee, code_etape, code_etudiant, inscription_premiere, lib_session)), 0)
+})
+
+# test_that("Pas de doublons dans les tables de métadonnées", {
+#   expect_equal(nrow(patchr::duplicate(inscrits, annee, code_etape, code_etudiant, inscription_premiere)), 0)
+# })
