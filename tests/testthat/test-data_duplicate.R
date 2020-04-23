57e431500976f7c4326eb2f7867ae9d6e49ea326
stephLH <github@le-henanff.fr>
2020-04-23 11:17:38 +0200

    tests: correction inscrits_peda

--- tests/testthat/test-data_duplicate.R
+++ tests/testthat/test-data_duplicate.R
@@ -5,7 +5,7 @@ test_that("Pas de doublons dans les tables de données", {
   expect_equal(nrow(patchr::duplicate(inscrits, annee, code_etape, code_etudiant, inscription_premiere)), 0)
   expect_equal(nrow(patchr::duplicate(inscrits_cpge, annee, code_etape, code_etudiant, inscription_premiere)), 0)
   expect_equal(nrow(patchr::duplicate(inscrits_elp, annee, code_etape, code_etudiant, inscription_premiere, code_elp)), 0)
-  expect_equal(nrow(patchr::duplicate(inscrits_peda, annee, code_etape, code_etudiant, inscription_premiere)), 0)
+  expect_equal(nrow(patchr::duplicate(inscrits_peda, annee, code_etape, code_etudiant)), 0)
   expect_equal(nrow(patchr::duplicate(resultats_elp, annee, code_etape, code_etudiant, inscription_premiere, code_elp, lib_session)), 0)
   expect_equal(nrow(patchr::duplicate(resultats_etape, annee, code_etape, code_etudiant, inscription_premiere, lib_session)), 0)
   expect_equal(nrow(patchr::duplicate(resultats_diplome, annee, code_etape, code_etudiant, inscription_premiere, lib_session)), 0)
